fe1c41ddb1679e241b593c779364b84044b43e90
Eduardo Amroginski Santos <eduardo.amroginski@pampa.compasso>
2021-05-14 18:04:54 -0300

    Adicionando chamada para a API, e testando o metodo Get

--- App.tsx
+++ App.tsx
@@ -1,13 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { FlatList, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import api from './src/services/api';
+
+
+interface PorkemonProps {
+  name: string;
+  url: string;
+}
 
 export default function App() {
+  const [ pokemons, setPokemons] = useState<PorkemonProps[]>([]);
+  const [ loading, setLoading ] = useState(true);
+
+  async function fetchPokemon(){
+    const { data } = await api.get(`pokemon`);
+    
+    if(!data.results) return setLoading(true);
+
+    setPokemons(data.results);
+
+    setLoading(false);
+
+  }
+
+  useEffect(() => { 
+    fetchPokemon()
+  }, [])
+
   return (
-    <View style={styles.container}>
-      <Text>Teste poke-app</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Text>Teste poke-app</Text>
+      </View>
+      <View style={styles.pokemons}>
+        <FlatList
+          data={pokemons}
+          keyExtractor={( item, key ) => String(key)}
+          renderItem={
+            ({ item }) => (
+              <>
+                <Text>{ item.name }</Text>
+                <Text>{ item.url }</Text>
+              </>
+            )
+          }
+          />
+      </View>
+    </SafeAreaView>
   );
 }
 
@@ -18,4 +58,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  pokemons: {
+    flex: 1,
+    paddingHorizontal: 32,
+    justifyContent: 'center'
+  },
+  pokemonList: {
+    flex: 1
+  }
 });
